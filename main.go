0be44671da3990c4202e54413c740df779fe18d8
default <joshua.adeyemi@stu.ocsb.ca>
2025-12-14 23:58:15 +0000

    commit message

---
+++ main.go
@@ -0,0 +1,38 @@
+package ics3uvunit4activity1assignment1
+
+// Author: Joshua Adeyemi
+// Version: 1.0.0
+// Date: 2025-12-14
+// FileOverview: This program calculates the average of student marks and gives performance feedback.
+
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Hardcoded input for portfolio/linter purposes
+	numMarks := 3
+	marks := []float64{75.8, 90.2, 80}
+
+	sum := 0.0
+	for i := 0; i < numMarks; i++ {
+		sum += marks[i]
+	}
+
+	average := sum / float64(numMarks)
+
+	fmt.Printf("You have entered %d marks. The student's average is %.0f%%.\n", numMarks, average)
+
+	if average <= 49 {
+		fmt.Println("The student is failing.")
+	} else if average <= 69 {
+		fmt.Println("The student's performance is just under average.")
+	} else if average <= 79 {
+		fmt.Println("The student's performance is average.")
+	} else {
+		fmt.Println("The student is on the honour roll.")
+	}
+
+	fmt.Println("\nDone.")
+}
